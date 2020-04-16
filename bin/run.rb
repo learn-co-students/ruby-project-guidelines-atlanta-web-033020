fa8a046a0b96d2d50bdcfbac37c14bff382185d0
github username <github email address>
2020-04-16 18:41:50 -0400

    Moved outer application menu/welcome to new App class. Updated run.rb to call App.run

--- bin/run.rb
+++ bin/run.rb
@@ -1,17 +1,3 @@
 require_relative '../config/environment'
 
-quit_program = false
-while !quit_program
-  # Welcome User
-  while !LoggedInUser.user && !LoggedInUser.shelter do
-    if welcome == "quit"
-      quit_program = true
-      break
-    end
-  end
-
-  # Cycle main menu until quit by user
-  while LoggedInUser.user || LoggedInUser.shelter do
-    main_menu
-  end
-end
+App.run
